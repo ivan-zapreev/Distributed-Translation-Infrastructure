bd4f2c2b42438114757bfd2b397d1fbca1592611
Dr. Ivan S. Zapreev <ivan-zapreev@users.noreply.github.com>
2015-08-15 16:39:29 +0200

    Extended the greedy allocator storage with the possibility to extend memory! Fixing a bug, adding logging.

--- inc/GreedyMemoryStorage.hpp
+++ inc/GreedyMemoryStorage.hpp
@@ -128,7 +128,7 @@ namespace uva {
                             _allocBytes += num;
                         } else {
                             //WARNING: If we need more space we allocate it additionally in a not so efficient way.
-                            LOG_INFO << "Allocating additional: " << num << " bytes!" << END_LOG;
+                            LOG_DEBUG3 << "Allocating additional: " << num << " bytes!" << END_LOG;
                             ptr = static_cast<void*> (new TStorageData[num]);
                             _memoryBuffers.push_back(ptr);
                         }
