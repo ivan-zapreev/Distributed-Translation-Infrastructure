47cd1567503e1fd797d1d1a7f9d24ca2e9cd9950
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2015-08-27 16:34:35 +0200

    Increased debug level for debugging.

--- inc/Globals.hpp
+++ inc/Globals.hpp
@@ -57,7 +57,7 @@ namespace uva {
             };
 
             //Defines the maximum logging level
-            static const DebugLevelsEnum LOGER_MAX_LEVEL = INFO3;
+            static const DebugLevelsEnum LOGER_MAX_LEVEL = DEBUG3;
             
             //Defines the log level from which the detailed timing info is available
             static const DebugLevelsEnum PROGRESS_ACTIVE_LEVEL = INFO2;
