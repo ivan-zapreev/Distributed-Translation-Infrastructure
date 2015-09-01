ae497851337eb1e9796abc0de376f32c6a1e73d6
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2015-09-01 17:01:18 +0200

    improving the comparison script.

--- test-data/diff_tries.sh
+++ test-data/diff_tries.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
-../dist/Release__Linux_/back-off-language-model-smt e_00_1000.lm test.txt c2wa info3 | grep -v "CPU" | grep -v "vmsize" | grep -v "Cultivating" > release.c2wa.out
-../dist/Release__Linux_/back-off-language-model-smt e_00_1000.lm test.txt cmhm info3  | grep -v "CPU" | grep -v "vmsize" | grep -v "Cultivating" > release.cmhm.out
-../dist/Release__Linux_/back-off-language-model-smt e_00_1000.lm test.txt w2ca info3 | grep -v "CPU" | grep -v "vmsize" | grep -v "Cultivating" | grep -v "' memory allocation strategy." > release.w2ca.out
-../dist/Release__Linux_/back-off-language-model-smt e_00_1000.lm test.txt w2ch info3 | grep -v "CPU" | grep -v "vmsize" | grep -v "Cultivating" > release.w2ch.out
-../dist/Release__Linux_/back-off-language-model-smt e_00_1000.lm test.txt c2dh info3 | grep -v "CPU" | grep -v "vmsize" | grep -v "Cultivating" > release.c2dh.out
+
+NUM_PARAMS=3
+if [ "$#" -ne ${NUM_PARAMS} ]; then
+   echo "Illegal number of parameters: $# expected ${NUM_PARAMS}"
+   echo "1 - the platform: {Linux, MaxOS, Centos}"
+   echo "2 - the model to run on"
+   echo "3 - the query input file to use"
+   exit 1
+fi
+
+../dist/Release__${1}_/back-off-language-model-smt ${2} ${3} c2wa info3 | grep -v "CPU" | grep -v "vmsize" | grep -v "Cultivating" > release.c2wa.out
+../dist/Release__${1}_/back-off-language-model-smt ${2} ${3} cmhm info3  | grep -v "CPU" | grep -v "vmsize" | grep -v "Cultivating" > release.cmhm.out
+../dist/Release__${1}_/back-off-language-model-smt ${2} ${3} w2ca info3 | grep -v "CPU" | grep -v "vmsize" | grep -v "Cultivating" | grep -v "' memory allocation strategy." > release.w2ca.out
+../dist/Release__${1}_/back-off-language-model-smt ${2} ${3} w2ch info3 | grep -v "CPU" | grep -v "vmsize" | grep -v "Cultivating" > release.w2ch.out
+../dist/Release__${1}_/back-off-language-model-smt ${2} ${3} c2dh info3 | grep -v "CPU" | grep -v "vmsize" | grep -v "Cultivating" > release.c2dh.out
 
 echo "\n----> c2wa vs. cmhm"
 diff release.c2wa.out release.cmhm.out > diff.c2wa.cmhm.out
