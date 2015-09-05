a07d2462455c80a908afb36f480994b93ff4bfba
Dr. Ivan S. Zapreev <ivan-zapreev@users.noreply.github.com>
2015-09-05 22:33:16 +0200

    Small fixes of the comparison script.

--- test-data/diff_tries.sh
+++ test-data/diff_tries.sh
@@ -12,7 +12,7 @@ fi
 echo "c2wa"
 ../dist/Release__${1}_/back-off-language-model-smt ${2} ${3} c2wa info3 | grep -v "CPU" | grep -v "vmsize" | grep -v "Cultivating" > release.c2wa.out
 echo "c2dm"
-../dist/Release__${1}_/back-off-language-model-smt ${2} ${3} c2dm info3  | grep -v "CPU" | grep -v "vmsize" | grep -v "Cultivating" > release.c2dm.out
+../dist/Release__${1}_/back-off-language-model-smt ${2} ${3} c2dm info3  | grep -v "CPU" | grep -v "vmsize" | grep -v "Cultivating" | grep -v "Gram ctx hash" > release.c2dm.out
 echo "w2ca"
 ../dist/Release__${1}_/back-off-language-model-smt ${2} ${3} w2ca info3 | grep -v "CPU" | grep -v "vmsize" | grep -v "Cultivating" | grep -v "' memory allocation strategy." > release.w2ca.out
 echo "w2ch"
@@ -22,7 +22,7 @@ echo "c2dh"
 
 echo "\n----> c2wa vs. c2dm"
 diff release.c2wa.out release.c2dm.out > diff.c2wa.c2dm.out
-cat diff.c2wa.cmhm.out
+cat diff.c2wa.c2dm.out
 
 echo "\n----> c2wa vs. w2ca"
 diff release.c2wa.out release.w2ca.out > diff.c2wa.w2ca.out
