719a91827ad3f8a1ce819251f39633a0dd14abf0
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2015-10-29 11:24:41 +0100

    working on the base M-gram class, implementing the method that computed hashes.

--- inc/BaseMGram.hpp
+++ inc/BaseMGram.hpp
@@ -64,7 +64,9 @@ namespace uva {
                      * @param word_index the used word index
                      * @param actual_level the actual level of the m-gram that will be used should be <= MAX_LEVEL_CAPACITY
                      */
-                    T_Base_M_Gram(WordIndexType & word_index, TModelLevel actual_level) : m_word_index(word_index), m_actual_level(actual_level) {
+                    T_Base_M_Gram(WordIndexType & word_index, TModelLevel actual_level)
+                    : m_word_index(word_index), m_actual_level(actual_level),
+                    m_actual_end_word_idx(actual_level - 1) {
                         if (DO_SANITY_CHECKS && (m_actual_level > MAX_LEVEL_CAPACITY)) {
                             stringstream msg;
                             msg << "The provided actual level: " << SSTR(m_actual_level)
@@ -81,7 +83,9 @@ namespace uva {
                      * undefined. Filling in the M-gram tokens is done elsewhere.
                      * @param word_index the used word index
                      */
-                    T_Base_M_Gram(WordIndexType & word_index) : m_word_index(word_index), m_actual_level(M_GRAM_LEVEL_UNDEF) {
+                    T_Base_M_Gram(WordIndexType & word_index)
+                    : m_word_index(word_index), m_actual_level(M_GRAM_LEVEL_UNDEF),
+                    m_actual_end_word_idx(M_GRAM_LEVEL_UNDEF) {
                     }
 
                     /**
@@ -96,7 +100,7 @@ namespace uva {
                      * The basic to string conversion operator for the m-gram
                      */
                     inline operator string() const {
-                        return tokens_to_string(m_tokens, get_first_word_index(MAX_LEVEL_CAPACITY, m_actual_level, false), get_last_word_index(MAX_LEVEL_CAPACITY, m_actual_level, false));
+                        return tokens_to_string(m_tokens, m_actual_begin_word_idx, m_actual_end_word_idx);
                     };
 
                 protected:
@@ -109,35 +113,10 @@ namespace uva {
                     //Stores the actual m-gram level, the number of meaningful elements in the tokens, the value of m for the m-gram
                     TModelLevel m_actual_level;
 
-                    /**
-                     * Allows to get the first word index within the tokens of word id array for the given parameters
-                     * @param PREFIX_LENGTH the considered m-gram prefix length
-                     * @param CURR_LEVEL the current sub-m-gram  level
-                     * @return the array index for the first word of this m-gram
-                     */
-                    constexpr static inline TModelLevel get_first_word_index(const TModelLevel PREFIX_LENGTH, const TModelLevel CURR_LEVEL){
-                        return (PREFIX_LENGTH - CURR_LEVEL);
-                    }
-
-                    /**
-                     * Allows to get the last word index within the tokens of word id array for the given parameters
-                     * @param PREFIX_LENGTH the considered m-gram prefix length
-                     * @param CURR_LEVEL the current sub-m-gram  level
-                     * @return the array index for the last word of this m-gram
-                     */
-                    constexpr static inline TModelLevel get_last_word_index(const TModelLevel PREFIX_LENGTH, const TModelLevel CURR_LEVEL) {
-                        return (PREFIX_LENGTH - 1);
-                    }
-
-                    /**
-                     * Allows to get the length of the sub-m-gram for the given parameters
-                     * @param PREFIX_LENGTH the considered m-gram prefix length
-                     * @param CURR_LEVEL the current sub-m-gram  level
-                     * @return the length of the sub-m-gram
-                     */
-                    constexpr static inline TModelLevel get_sub_gram_length(const TModelLevel PREFIX_LENGTH, const TModelLevel CURR_LEVEL) {
-                        return CURR_LEVEL;
-                    }
+                    //These variables store the actual begin and end word index
+                    //for all the words of this M-gram stored in the internal arrays
+                    constexpr static TModelLevel m_actual_begin_word_idx = 0;
+                    TModelLevel m_actual_end_word_idx;
 
                     /**
                      * Allows to create a new m-gram id for the sub-m-gram defined by the given of the method template parameters.
@@ -145,59 +124,94 @@ namespace uva {
                      * a) If there was no memory allocated for the M-gram id then there will be allocated as much
                      * as needed to store the given id.
                      * b) If there was memory allocated then no re-allocation will be done, then it is assumed that enough memory was allocated
-                     * @param PREFIX_LENGTH the length of the currently considered M-gram prefix
-                     * @param CURR_LEVEL the level of the considered sub-m-gram in the considered m-gram prefix
+                     * @param BEGIN_WORD_IDX the index of the first word in the sub-m-gram, indexes start with 0
+                     * @param END_WORD_IDX the index of the last word in the sub-m-gram, indexes start with 0
                      * @param word_ids the list of the word ids for the entire m-gram, where at least the m-gram word
                      *                 ids for the sub-m-gram defined by the template parameters are known and initialized. 
                      * @param p_m_gram_id the reference to the M-gram id data pointer to be initialized with the M-gram id data, must be pre-allocated
                      */
-                    template<TModelLevel PREFIX_LENGTH, TModelLevel CURR_LEVEL >
+                    template<TModelLevel BEGIN_WORD_IDX, TModelLevel END_WORD_IDX >
                     inline void create_m_gram_id(const TWordIdType word_ids[MAX_LEVEL_CAPACITY], T_Gram_Id_Data_Ptr & p_m_gram_id) const {
-                        //Compute the first index based on the input template parameters
-                        constexpr TModelLevel FIRST_WORD_IDX = get_first_word_index(PREFIX_LENGTH, CURR_LEVEL);
-                        //Compute the length based on the input template parameters
-                        constexpr TModelLevel SUB_GRAM_LEN = get_sub_gram_length(PREFIX_LENGTH, CURR_LEVEL);
+                        //Compute the number of words based on the input template parameters
+                        constexpr TModelLevel NUMBER_OF_WORDS = END_WORD_IDX - BEGIN_WORD_IDX + 1;
 
-                        LOG_DEBUG << "Computing sub " << SSTR(CURR_LEVEL) << "-gram id for the m-gram defined by the first"
-                                << " word index: " << SSTR(FIRST_WORD_IDX) << ", and length: " << SSTR(SUB_GRAM_LEN) << END_LOG;
+                        LOG_DEBUG << "Computing sub " << SSTR(NUMBER_OF_WORDS) << "-gram id for the gram "
+                                << "defined by the first, and the last word indexes: ["
+                                << SSTR(BEGIN_WORD_IDX) << ", " << SSTR(END_WORD_IDX) << "]" << END_LOG;
 
                         //Create the M-gram id from the word ids.
-                        T_M_Gram_Id::template create_m_gram_id<FIRST_WORD_IDX, SUB_GRAM_LEN>(word_ids, p_m_gram_id);
+                        T_M_Gram_Id::template create_m_gram_id<BEGIN_WORD_IDX, NUMBER_OF_WORDS>(word_ids, p_m_gram_id);
 
-                        //Compute the last index based on the input template parameters, is only needed for logging
-                        constexpr TModelLevel LAST_WORD_IDX = get_last_word_index(PREFIX_LENGTH, CURR_LEVEL);
-                        LOG_DEBUG << "Allocated " << CURR_LEVEL << "-gram id is: " << (void*) p_m_gram_id
-                                << " for " << tokens_to_string(m_tokens, FIRST_WORD_IDX, LAST_WORD_IDX) << END_LOG;
+                        //Log the result
+                        LOG_DEBUG << "Allocated " << NUMBER_OF_WORDS << "-gram id is: " << (void*) p_m_gram_id
+                                << " for " << tokens_to_string(m_tokens, BEGIN_WORD_IDX, END_WORD_IDX) << END_LOG;
                     }
 
                     /**
                      * Allows to compute the hash for the given sub-m-gram that is defined by the
-                     * given of the method template parameters.
-                     * The hash computation is stopped as soon as an <unk> word is encountered.
-                     * The hash is computed incrementally from the last word id and then up until
-                     * the first word id of the sub-m-gram defined by the template parameters.
-                     * @param PREFIX_LENGTH the length of the currently considered M-gram prefix
-                     * @param CURR_LEVEL the level of the considered sub-m-gram in the considered m-gram prefix
+                     * given of the method template parameters. The hash is computed incrementally
+                     * from the last word id and then up until the first word idx of the sub-m-gram
+                     * defined by the template parameters. The hash computation is stopped as soon
+                     * as an <unk> word is encountered.
+                     * @param BEGIN_WORD_IDX the index of the first word in the sub-m-gram, indexes start with 0
+                     * @param END_WORD_IDX the index of the last word in the sub-m-gram, indexes start with 0
                      * @param word_ids the list of the word ids for the entire m-gram, where at least the m-gram word
                      *                 ids for the sub-m-gram defined by the template parameters are known and initialized. 
                      * @param hash_values the array of hash values to be filled in with hashes
+                     * @return true if all the requested word hashes could be computed otherwise false
                      */
-                    template<TModelLevel PREFIX_LENGTH, TModelLevel C URR_LEVEL>
-                    inline void compute_hashes(const TWordIdType word_ids[MAX_LEVEL_CAPACITY], uint64_t hash_values[MAX_LEVEL_CAPACITY]) const {
-                        //Compute the first index based on the input template parameters
-                        constexpr TModelLevel FIRST_WORD_IDX = get_first_word_index(PREFIX_LENGTH, CURR_LEVEL);
-                        //Compute the last index based on the input template parameters
-                        constexpr TModelLevel LAST_WORD_IDX = get_last_word_index(PREFIX_LENGTH, CURR_LEVEL);
+                    template<TModelLevel BEGIN_WORD_IDX, TModelLevel END_WORD_IDX >
+                    inline bool compute_hashes(const TWordIdType word_ids[MAX_LEVEL_CAPACITY], uint64_t hash_values[MAX_LEVEL_CAPACITY]) const {
+                        //Declare and default initialize the result variable
+                        bool result = true;
+                        //Compute the number of words based on the input template parameters
+                        constexpr TModelLevel NUMBER_OF_WORDS = END_WORD_IDX - BEGIN_WORD_IDX + 1;
 
-                        LOG_DEBUG << "Computing sub " << SSTR(CURR_LEVEL) << "-gram hash for the m-gram "
+                        LOG_DEBUG << "Computing sub " << SSTR(NUMBER_OF_WORDS) << "-gram hash for the gram "
                                 << "defined by the first, and the last word indexes: ["
-                                << SSTR(FIRST_WORD_IDX) << ", " << SSTR(LAST_WORD_IDX) << "]" << END_LOG;
-                        
-                        //ToDo: Implement
+                                << SSTR(BEGIN_WORD_IDX) << ", " << SSTR(END_WORD_IDX) << "]" << END_LOG;
+
+                        //First check if the end word is not unknown, if yes then there is no need to compute hashes
+                        TModelLevel curr_idx = END_WORD_IDX;
+                        if (word_ids[curr_idx] != WordIndexType::UNKNOWN_WORD_ID) {
+                            //If the word is not unknown then the first hash, the word's hash is its id
+                            hash_values[curr_idx] = word_ids[curr_idx];
+
+                            //Iterate through the remaining word ids, if any, and build-up hashes
+                            if (curr_idx > BEGIN_WORD_IDX) {
+                                do {
+                                    //Decrement the word id
+                                    curr_idx--;
+
+                                    //Check if the next word id is unknown, if yes then just stop iterations
+                                    if (word_ids[curr_idx] != WordIndexType::UNKNOWN_WORD_ID) {
+                                        //Incrementally build up hash, using the previous hash value and the next word id
+                                        hash_values[curr_idx] = combine_hash(word_ids[curr_idx], hash_values[curr_idx + 1]);
+                                    } else {
+                                        //We did not compute all the required hashed because of <unk>
+                                        result = false;
+                                        //Stop iterations
+                                        break;
+                                    }
+                                } while (curr_idx != BEGIN_WORD_IDX);
+                            }
+
+                            //Log the result
+                            LOG_DEBUG << "Computed " << NUMBER_OF_WORDS << "-gram hash is: " << word_ids[curr_idx]
+                                    << " for the maximum sub-m-gram: " << tokens_to_string(m_tokens, curr_idx, END_WORD_IDX) << END_LOG;
+                        } else {
+                            //We did not compute all the required hashed because of <unk>
+                            result = false;
+                            
+                            //Log the result
+                            LOG_DEBUG << "Computed " << NUMBER_OF_WORDS << "-gram hash is: NONE, the word: "
+                                    << tokens_to_string(m_tokens, END_WORD_IDX, END_WORD_IDX) << " with index "
+                                    << SSTR(END_WORD_IDX) << " is an <unk>word!" << END_LOG;
+                        }
+
+                        return result;
                     }
-
                 };
-
             }
         }
     }
