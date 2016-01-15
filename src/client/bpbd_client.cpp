5a0502227966eafbcf5f43b4d9bcf141db61a81d
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2016-01-15 09:41:24 +0100

    adding program parameters to the translation client

--- src/client/bpbd_client.cpp
+++ src/client/bpbd_client.cpp
@@ -48,7 +48,9 @@ using namespace uva::utils::exceptions;
  */
 typedef struct {
     string file_in;
+    string lang_in;
     string file_out;
+    string lang_out;
     string host;
     uint16_t port;
 } TExecutionParams;
@@ -60,18 +62,62 @@ static void print_info() {
     print_info("The translation client application", PROGRAM_VERSION_STR);
 }
 
+//The pointer to the command line parameters parser
+static CmdLine * p_cmd_args = NULL;
+static ValueArg<string> * p_file_in_arg = NULL;
+static ValueArg<string> * p_lang_in_arg = NULL;
+static ValueArg<string> * p_file_out_arg = NULL;
+static ValueArg<string> * p_lang_out_arg = NULL;
+static ValueArg<string> * p_host_arg = NULL;
+static ValueArg<uint16_t> * p_port_arg = NULL;
+static vector<string> debug_levels;
+static ValuesConstraint<string> * p_debug_levels_constr = NULL;
+static ValueArg<string> * p_debug_level_arg = NULL;
+
 /**
  * Creates and sets up the command line parameters parser
  */
 void create_arguments_parser() {
-    //ToDo: Implement
+    //Declare the command line arguments parser
+    p_cmd_args = new CmdLine("", ' ', PROGRAM_VERSION_STR);
+
+    //Add the  parameter - compulsory
+    p_file_in_arg = new ValueArg<string>("if", "input-file", "A back-off language model file name in ARPA format", true, "", "model file name", *p_cmd_args);
+
+    //Add the  parameter - compulsory
+    p_lang_in_arg = new ValueArg<string>("il", "input-lang", "A back-off language model file name in ARPA format", true, "", "model file name", *p_cmd_args);
+
+    //Add the  parameter - compulsory
+    p_file_out_arg = new ValueArg<string>("of", "output-file", "A text file containing new line separated M-gram queries", true, "", "query file name", *p_cmd_args);
+
+    //Add the  parameter - optional, by default is "English"
+    p_lang_out_arg = new ValueArg<string>("ol", "output-lang", "A text file containing new line separated M-gram queries", false, "English", "query file name", *p_cmd_args);
+
+    //Add the  parameter - optional, by default is "localhost"
+    p_host_arg = new ValueArg<string>("h", "host", "A text file containing new line separated M-gram queries", false, "localhost", "query file name", *p_cmd_args);
+
+    //Add the  parameter - optional, by default is 9002
+    p_port_arg = new ValueArg<uint16_t>("p", "port", "A text file containing new line separated M-gram queries", false, 9002, "query file name", *p_cmd_args);
+
+    //Add the -d the debug level parameter - optional, default is e.g. RESULT
+    Logger::get_reporting_levels(&debug_levels);
+    p_debug_levels_constr = new ValuesConstraint<string>(debug_levels);
+    p_debug_level_arg = new ValueArg<string>("d", "debug", "The debug level to be used", false, RESULT_PARAM_VALUE, p_debug_levels_constr, *p_cmd_args);
 }
 
 /**
  * Allows to deallocate the parameters parser if it is needed
  */
 void destroy_arguments_parser() {
-    //ToDo: Implement
+    SAFE_DESTROY(p_file_in_arg);
+    SAFE_DESTROY(p_lang_in_arg);
+    SAFE_DESTROY(p_file_out_arg);
+    SAFE_DESTROY(p_lang_out_arg);
+    SAFE_DESTROY(p_host_arg);
+    SAFE_DESTROY(p_port_arg);
+    SAFE_DESTROY(p_debug_levels_constr);
+    SAFE_DESTROY(p_debug_level_arg);
+    SAFE_DESTROY(p_cmd_args);
 }
 
 /**
@@ -81,7 +127,25 @@ void destroy_arguments_parser() {
  * @param params the structure that will be filled in with the parsed program arguments
  */
 static void extract_arguments(const uint argc, char const * const * const argv, TExecutionParams & params) {
-    //ToDo: Implement
+    //Parse the arguments
+    try {
+        p_cmd_args->parse(argc, argv);
+    } catch (ArgException &e) {
+        stringstream msg;
+        msg << "Error: " << e.error() << ", for argument: " << e.argId();
+        throw Exception(msg.str());
+    }
+
+    //Store the parsed parameter values
+    params.file_in = p_file_in_arg->getValue();
+    params.lang_in = p_lang_in_arg->getValue();
+    params.file_out = p_file_out_arg->getValue();
+    params.lang_out = p_lang_out_arg->getValue();
+    params.host = p_host_arg->getValue();
+    params.port = p_port_arg->getValue();
+
+    //Set the logging level right away
+    Logger::set_reporting_level(p_debug_level_arg->getValue());
 }
 
 /**
