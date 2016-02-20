da313ce8177d1bdea4b2e2366233e13b747cb50b
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2016-02-20 11:00:13 +0100

    Done:   A. Minor fixes in the output Still need to:   2. Compute future costs when preparing the sentence for translation     2.1. Extend the LM query to go beyond the 5-gram and have a sliding window     2.1. Make the TM to count the number of target words and to store them in          a data structure easy for combining with history and querying the LM

--- inc/common/utils/monitore/statistics_monitore.hpp
+++ inc/common/utils/monitore/statistics_monitore.hpp
@@ -101,31 +101,31 @@ namespace uva {
             /**
              * This function is meant to give the memory statistics information delta
              * @param action the monitored action
-             * @param msStart the start memory usage statistics
-             * @param msEnd the end memory usage statistics
-             * @param isDoInfo true if the memory info may be print
+             * @param ms_start the start memory usage statistics
+             * @param ms_end the end memory usage statistics
+             * @param is_do_info true if the memory info may be print
              */
-            static inline void report_memory_usage(const char* action, TMemotyUsage msStart, TMemotyUsage msEnd, const bool isDoInfo) {
+            static inline void report_memory_usage(const char* action, TMemotyUsage ms_start, TMemotyUsage ms_end, const bool is_do_info) {
                 LOG_USAGE << "Action: \'" << action << "\' memory change:" << END_LOG;
-                LOG_DEBUG << "\tmemory before: vmsize=" << SSTR(msStart.vmsize) << " Kb, vmpeak="
-                        << SSTR(msStart.vmpeak) << " Kb, vmrss=" << SSTR(msStart.vmrss)
-                        << " Kb, vmhwm=" << SSTR(msStart.vmhwm) << " Kb" << END_LOG;
-                LOG_DEBUG << "memory after: vmsize=" << SSTR(msEnd.vmsize) << " Kb, vmpeak="
-                        << SSTR(msEnd.vmpeak) << " Kb, vmrss=" << SSTR(msEnd.vmrss)
-                        << " Kb, vmhwm=" << SSTR(msEnd.vmhwm) << " Kb" << END_LOG;
+                LOG_DEBUG << "\tmemory before: vmsize=" << SSTR(ms_start.vmsize) << " Kb, vmpeak="
+                        << SSTR(ms_start.vmpeak) << " Kb, vmrss=" << SSTR(ms_start.vmrss)
+                        << " Kb, vmhwm=" << SSTR(ms_start.vmhwm) << " Kb" << END_LOG;
+                LOG_DEBUG << "memory after: vmsize=" << SSTR(ms_end.vmsize) << " Kb, vmpeak="
+                        << SSTR(ms_end.vmpeak) << " Kb, vmrss=" << SSTR(ms_end.vmrss)
+                        << " Kb, vmhwm=" << SSTR(ms_end.vmhwm) << " Kb" << END_LOG;
 
-                int vmsize = ((msEnd.vmsize < msStart.vmsize) ? 0 : msEnd.vmsize - msStart.vmsize) / BYTES_ONE_MB;
-                int vmpeak = ((msEnd.vmpeak < msStart.vmpeak) ? 0 : msEnd.vmpeak - msStart.vmpeak) / BYTES_ONE_MB;
-                int vmrss = ((msEnd.vmrss < msStart.vmrss) ? 0 : msEnd.vmrss - msStart.vmrss) / BYTES_ONE_MB;
-                int vmhwm = ((msEnd.vmhwm < msStart.vmhwm) ? 0 : msEnd.vmhwm - msStart.vmhwm) / BYTES_ONE_MB;
+                int vmsize = ((ms_end.vmsize < ms_start.vmsize) ? 0 : ms_end.vmsize - ms_start.vmsize) / BYTES_ONE_MB;
+                int vmpeak = ((ms_end.vmpeak < ms_start.vmpeak) ? 0 : ms_end.vmpeak - ms_start.vmpeak) / BYTES_ONE_MB;
+                int vmrss = ((ms_end.vmrss < ms_start.vmrss) ? 0 : ms_end.vmrss - ms_start.vmrss) / BYTES_ONE_MB;
+                int vmhwm = ((ms_end.vmhwm < ms_start.vmhwm) ? 0 : ms_end.vmhwm - ms_start.vmhwm) / BYTES_ONE_MB;
                 LOG_USAGE << showpos << "vmsize=" << vmsize << " Mb, vmpeak=" << vmpeak
                         << " Mb, vmrss=" << vmrss << " Mb, vmhwm=" << vmhwm
                         << " Mb" << noshowpos << END_LOG;
-                if (isDoInfo) {
-                    LOG_INFO << "  vmsize - Virtual memory size; vmpeak - Peak virtual memory size" << END_LOG;
-                    LOG_INFO << "    Virtual memory size is how much virtual memory the process has in total (RAM+SWAP)" << END_LOG;
-                    LOG_INFO << "  vmrss  - Resident set size; vmhwm  - Peak resident set size" << END_LOG;
-                    LOG_INFO << "    Resident set size is how much memory this process currently has in main memory (RAM)" << END_LOG;
+                if (is_do_info) {
+                    LOG_INFO3 << "  vmsize - Virtual memory size; vmpeak - Peak virtual memory size" << END_LOG;
+                    LOG_INFO3 << "    Virtual memory size is how much virtual memory the process has in total (RAM+SWAP)" << END_LOG;
+                    LOG_INFO3 << "  vmrss  - Resident set size; vmhwm  - Peak resident set size" << END_LOG;
+                    LOG_INFO3 << "    Resident set size is how much memory this process currently has in main memory (RAM)" << END_LOG;
                 }
             }
         }
