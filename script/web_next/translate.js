631f060c8e5382630c72d60f4438e8ccb48fed0c
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2016-08-19 16:20:23 +0200

    Starting on the pre/post- processor extended version of the UI.

---
+++ script/web_next/translate.js
@@ -0,0 +1,27 @@
+
+/**
+ * This function is to be called when the pre-processor server url is chaned.
+ */
+function on_pre_server_change() {
+    "use strict";
+    
+    //ToDo: Implement
+}
+
+/**
+ * This function is to be called when the translaton server url is chaned.
+ */
+function on_trans_server_change() {
+    "use strict";
+    
+    //ToDo: Implement
+}
+
+/**
+ * This function is to be called when the post-processor server url is chaned.
+ */
+function on_post_server_change() {
+    "use strict";
+    
+    //ToDo: Implement
+}
