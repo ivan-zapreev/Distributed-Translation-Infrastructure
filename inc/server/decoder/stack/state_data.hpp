8b0424d093a6f90cca78ac61dc4f8661818e6464
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2016-09-09 11:26:02 +0200

    Removed error logging for empty target at things seem to work now. Disabled lattice generation.

--- inc/server/decoder/stack/state_data.hpp
+++ inc/server/decoder/stack/state_data.hpp
@@ -271,20 +271,6 @@ namespace uva {
                                         }
                                     }
                                 }
-                                
-                                //Temporary error logging
-                                if (is_lattice && (trim(storage) == "")) {
-                                    LOG_ERROR << "Empty translation string, target: " <<
-                                            (m_target != NULL ?
-                                            (m_target->is_unk_trans() ? string("UNK wd[") +
-                                            to_string(m_s_begin_word_idx) + string(",") + to_string(m_s_end_word_idx) + string("] ch(") +
-                                            to_string(m_stack_data.m_sent_data[m_s_begin_word_idx][m_s_begin_word_idx].m_begin_ch_idx)
-                                            + string(",") +
-                                            to_string(m_stack_data.m_sent_data[m_s_end_word_idx][m_s_end_word_idx].m_end_ch_idx) +
-                                            string("), text: '") + m_stack_data.m_source_sent + string("'") :
-                                            m_target->get_target_phrase())
-                                            : "NULL") << END_LOG;
-                                }
 
                                 LOG_DEBUG1 << "The END storage value is: ___" << storage << "___" << END_LOG;
                             }
