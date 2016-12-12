ee0ee5333076cab693fecb73ed76acfae3de63e9
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2016-12-12 13:48:57 +0100

    Working on the tuning script to log the progress and extract the best scoring config

--- script/tuning/run_tuning.sh
+++ script/tuning/run_tuning.sh
@@ -2,15 +2,16 @@
 
 ###################################
 #Declare and set the default values of the script parameters
-CONFIG_FILE_NAME="" #No default value, is a compulsory parameter
-SOURCE_TEXT_FILE="" #No default value, is a compulsory parameter
-SOURCE_LANG=""
-NUM_BATCHES=1 #Defaul is no concurrency and multi-threading in tuning scripts, just one batch
-REFERENCE_FILE_PREF="" #No default value, is a compulsory parameter
-REFERENCE_LANGUAGE="english" #The default target language of translation
-TRACE_LEVEL=1 #Default is one (1) the lowest (?) tracing level
-NUM_BEST_HYPOTHESIS=1500 #In our research we use more than a 1000 hypothesis
-MERT_SCRIPT_TYPE="PRO-14" #In our research we use the PRO-14 test
+export CONFIG_FILE_NAME="" #No default value, is a compulsory parameter
+export SOURCE_TEXT_FILE="" #No default value, is a compulsory parameter
+export SOURCE_LANG=""
+export NUM_BATCHES=1 #Defaul is no concurrency and multi-threading in tuning scripts, just one batch
+export REFERENCE_FILE_PREF="" #No default value, is a compulsory parameter
+export REFERENCE_LANGUAGE="english" #The default target language of translation
+export TRACE_LEVEL=1 #Default is one (1) the lowest (?) tracing level
+export NUM_BEST_HYPOTHESIS=1500 #In our research we use more than a 1000 hypothesis
+export MERT_SCRIPT_TYPE="PRO-14" #In our research we use the PRO-14 test
+export STOP_TUNING_SCRIPT=false #The tuning process stop script flag
 
 #Prints the program usage
 # ${0} - the program name
@@ -50,6 +51,30 @@ function error() {
     exit 1
 }
 
+#Allows to run the tuning script in parallel to the rest of the script
+do_tuning() {
+    $BASEDIR/scripts/tuner.pl --src=${SOURCE_TEXT_FILE} --node-scoring --ref="${REFERENCE_FILE_PREF}." --decoder=$BASEDIR/start_infra.sh --external-path=${MEGA_M_HOME_DIR} --conf=${CONFIG_FILE_NAME} --no-parallel=${NUM_BATCHES} --trace=${TRACE_LEVEL} --nbest-size=${NUM_BEST_HYPOTHESIS} --src-language=${SOURCE_LANG} --mert-script=${MERT_SCRIPT_TYPE} --trg-language=${REFERENCE_LANGUAGE} --experiment-dir="." 1>${TUNING_LOG_FILE} 2>&1 &
+
+    #Remember the java process id
+    TUNING_PROCESS_ID=$!
+    #Disown the process to prevent the killed message printed
+    disown
+
+    #Wait until the file is created
+    while [ "${STOP_TUNING_SCRIPT}" = false ]; do
+        #ToDo: Provide the progress infor of the tuning, iteration, bleu score
+        
+        #Wait until the new iteration
+        sleep 1
+    done
+    
+    #Kill the tuning process and its children
+    kill -- -"$(ps -o pgid=${TUNING_PROCESS_ID} | tr -d ' ' | grep [0-9])"
+    wait ${TUNING_PROCESS_ID}
+}
+export -f do_tuning
+
+###################################
 #If there is no arguments then print usage and exit
 if [ "$#" -eq 0 ]; then
     error
@@ -139,12 +164,28 @@ fi
 unset LANG
 
 #Get the directory where this script is located
-BASEDIR=$(dirname "$0")
+export BASEDIR=$(dirname "$0")
 
 #Define the folder where the Mega-m is located
-MEGA_M_HOME_DIR=$BASEDIR/megam_0.92/
+export MEGA_M_HOME_DIR=$BASEDIR/megam_0.92/
 
 DATE_TIME=`date`
 echo "Starting tuning on: ${HOSTNAME} at: ${DATE_TIME}"
+export TUNING_LOG_FILE="tuning.log"
+echo "Tuning log file: ${TUNING_LOG_FILE}"
 
-$BASEDIR/scripts/tuner.pl --src=${SOURCE_TEXT_FILE} --node-scoring --ref="${REFERENCE_FILE_PREF}." --decoder=$BASEDIR/start_infra.sh --external-path=${MEGA_M_HOME_DIR} --conf=${CONFIG_FILE_NAME} --no-parallel=${NUM_BATCHES} --trace=${TRACE_LEVEL} --nbest-size=${NUM_BEST_HYPOTHESIS} --src-language=${SOURCE_LANG} --mert-script=${MERT_SCRIPT_TYPE} --trg-language=${REFERENCE_LANGUAGE} --experiment-dir="."
+#Run the tuning in parallel
+do_tuning
+
+#Wait for the input to finish
+echo "Please press enter to finish..."
+read input_variable
+
+#Set the stopping flag to true
+STOP_TUNING_SCRIPT=true
+
+#Wait for all sub-processes to finish
+echo "Waiting for the sub-processes to finish..."
+wait
+
+#ToDo: Extract the best scorint configuration profile
