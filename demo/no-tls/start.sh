1ed282df0200ee9cedcdd7263711c19b12b72b09
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2018-05-30 12:09:14 +0200

    Making the new start script to work

--- demo/no-tls/start.sh
+++ demo/no-tls/start.sh
@@ -1,7 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 
 export PROJECT_PATH=../..
 export BINARY_PATH=${PROJECT_PATH}/build
+
+function extract_if_not_present() {
+    if [ -f "${1}" ]; then
+        echo "INFO: ${1} is already extracted!"
+    else
+        echo "INFO: Exracting ${1} ... "
+
+        #Define the requested archive name
+        REC_ARCH_NAME=${1}.zip
+
+        #Check if we have a multi-part archive, then
+        #we need to first restore the complete file
+        if [ ! -f "${REC_ARCH_NAME}" ]; then
+            ACT_ARCH_NAME=.${REC_ARCH_NAME}
+            #Combine the archive parts
+            cat ${REC_ARCH_NAME}.* > ${ACT_ARCH_NAME}
+        else
+            ACT_ARCH_NAME=${REC_ARCH_NAME}
+        fi
+
+        #Unzip the actual archive
+        unzip ${ACT_ARCH_NAME}
+
+        #Remove the actual archive if this is a multi-part one
+        if [ ! -f "${REC_ARCH_NAME}" ]; then
+            rm -f ${ACT_ARCH_NAME}
+        fi
+
+        echo "Done"
+    fi
+}
 
 echo "INFO: Using the project path: ${PROJECT_PATH}"
 
@@ -12,7 +43,12 @@ ln -f -s ${PROJECT_PATH}/script/text/post_process.sh .
 #Extracting the models
 echo "INFO: Starting Extracting the model files:"
 cd ./models
-unzip *.zip
+extract_if_not_present chinese.english.head.10.rm
+extract_if_not_present chinese.english.head.10.tm
+extract_if_not_present de-en-1-10.000.rm
+extract_if_not_present de-en-1-10.000.tm
+extract_if_not_present english.bitext.lm
+extract_if_not_present e_00_1000.lm
 cd ..
 
 #First start the servers
